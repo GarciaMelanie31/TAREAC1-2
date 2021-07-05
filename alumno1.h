102a64f6196c0f7419110ac75fec36b71ad45c65
GarciaMelanie31 <86171662+GarciaMelanie31@users.noreply.github.com>
2021-07-05 11:13:26 -0500

    Add files via upload

---
+++ alumno1.h
@@ -0,0 +1,43 @@
+void ingresar(float (*m1)[5], float(*m2)[5])
+{
+	int f,c;
+	//  cout<<"Ingrese el numero de filas: "<<endl;
+	  //cin>>f;
+	  //cout<<"Ingrese el numero de columnas: "<<endl;
+	  //cin>>c;
+	  //int matriz1[f][c], matriz2[f][c];
+	  cout<<endl;
+	  cout<<"Ingrese los valores de la matriz 1"<<endl;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Ingrese un numero: ";
+			cin>>m1[i][j];
+		}
+	}
+	cout<<"\n";
+	cout<<endl;
+	cout<<"Ingrese los valores de la matriz 2"<<endl;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Ingrese un numero: ";
+			cin>>m2[i][j];
+		}
+	}
+	cout<<"\n";
+	cout<<"La matriz 1 es:"<<endl;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<m1[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"\n";
+	cout<<"La matriz 2 es:"<<endl;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<m2[i][j];
+		}
+		cout<<endl;
+	}
+ 	
+}
