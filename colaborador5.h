e400679bf5c221edad72e6f9a398cb61b4675e78
JonathanJosue23 <86244258+JonathanJosue23@users.noreply.github.com>
2021-07-05 12:21:01 -0500

    Update colaborador5.h
    para determinar cual matriz tiene mayor media y varianza se realiza a separar las matrices la matriz numero 1 realizara la suma al igual que la matriz numero 2 para sacar la varianza se multiplicara la matriz y todo esto realizara una suma y dará el resultado de cual será el resultado

--- colaborador5.h
+++ colaborador5.h
@@ -1,9 +1,74 @@
-//COLABORADOR5 - INGRESES SU NOMBRE
+//COLABORADOR5 - Jonathan Ramirez Perea 
 void mediayvarianza(float (*m1)[5], float (*m2)[5])
-{
-  
-  
-  
-  
-  
+float media=0, media2=0, varianza1=0, varianza2=0;
+cout<<"\nel resultado de la media de la matriz 1 es:";
+
+for (int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		
+		media+= m1[i][j];
+		
+		
+	}
+
 }
+media/=25;
+
+cout<<media<<endl;
+
+cout<<"\nel resultado de la media de la matriz 2 es:";
+
+for (int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		
+		media2+= m2[i][j];
+	}
+
+}
+media2/=25;
+cout<<media2<<endl;
+
+cout<<"\nla varianza de la primera matriz es:";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		
+		varianza1+= (m1[i][j]-media)*(m1[i][j]-media);
+	}	
+}
+varianza1/=25;
+
+cout<<varianza1<<endl;
+
+cout<<"\nla varianza de la segunda matriz es:";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		
+		varianza2+= (m2[i][j]-media2)*(m2[i][j]-media2);
+	}	
+}
+varianza2/=25;
+
+cout<<varianza2<<endl<<endl;
+
+if(media>media2){
+	
+	cout<<"\nmedia de matriz 1 es mayor que la media de matriz 2\n\n";
+}
+else{
+	
+	cout<<"\nmedia de matriz 2 es mayor que la media de matriz 1\n\n";
+}
+if(varianza1>varianza2){
+	
+	cout<<"\nvarianza de matriz 1 es mayor que la varianza de matriz 2\n\n";
+}
+else{
+	
+	cout<<"\nvarianza de matriz 2 es mayor que la varianza de matriz 1\n\n";
+}
+  
+  
+  
+  
+  
+
